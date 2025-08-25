85c3d3664fdd6c4ec53a5bed06147e2671cbb7a2
josh-tustanowski-fdmt <josh@fundment.com>
2025-08-25 23:46:35 +0100

    refactor: change the interface to take in a client object with the transactions under each account

--- src/schema/isa_allowance.py
+++ src/schema/isa_allowance.py
@@ -1,24 +1,33 @@
 import enum
 from decimal import Decimal
+from typing import List, Set
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import datetime as dt
 
 
 class AccountType(enum.Enum):
-    ISA = enum.auto()  # non-flexible
+    Non_Flexible_ISA = enum.auto()  # non-flexible
     Flexible_ISA = enum.auto()
     Flexible_Lifetime_ISA = enum.auto()
 
-class Account(BaseModel):
-    id: int
-    client_id: int
-    account_type: AccountType
+    @classmethod
+    def get_isa_account_types(cls) -> Set["AccountType"]:
+        return {cls.Flexible_ISA, cls.Flexible_Lifetime_ISA, cls.Non_Flexible_ISA}
+        
 
 class Transaction(BaseModel):
-    account: Account
     transaction_date: dt.date
     amount: Decimal
+
+class Client(BaseModel):
+    id: int
+    accounts: List['Account'] = Field(default_factory=list)
+
+class Account(BaseModel):
+    id: int
+    account_type: AccountType
+    transactions: List[Transaction] = Field(default_factory=list)
 
 class TotalLimit(BaseModel):
     tax_year: str
--- src/service/isa_allowance.py
+++ src/service/isa_allowance.py
@@ -1,28 +1,44 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Set
 from decimal import Decimal
 
 from .constants import ISA_ANNUAL_ALLOWANCE, LISA_ANNUAL_ALLOWANCE
 
 from ..schema.isa_allowance import (
-    Account, IsaAllowance, Transaction, AccountType
+    Account,
+    IsaAllowance, 
+    AccountType,
+    Client,
+    Transaction,
 )
 
 IsaLimitMapping = Dict[AccountType, Decimal]
 
 def calculate_isa_allowance_for_account(
+    client: Client,
     account: Account,
-    transactions: List[Transaction],
     tax_year: int = 2024,
     annual_allowance_override: Optional[IsaLimitMapping] = None
 ) -> IsaAllowance:
     """
     Main entrypoint for calculating the ISA allowance for a given account.
+    The client object contains all accounts and their transactions.
     """
     limits = annual_allowance_override or get_isa_limits_for_tax_year(tax_year)
+    client_transactions = get_client_transactions(client, account_types=AccountType.get_isa_account_types())
+    # TODO: Put in place checks on the tax-year for each transaction.
     return IsaAllowance(
         annual_allowance=limits.get(account.account_type, Decimal("0")),
         remaining_allowance=Decimal("0")
     )
+
+
+def get_client_transactions(
+    client: Client, 
+    account_types: Set[AccountType],
+) -> List[Transaction]:
+    accounts = [account for account in client.accounts if account.account_type in account_types]
+    return [t for a in accounts for t in a.transactions]
+
 
 
 def get_isa_limits_for_tax_year(tax_year: int) -> IsaLimitMapping:
@@ -32,7 +48,7 @@ def get_isa_limits_for_tax_year(tax_year: int) -> IsaLimitMapping:
     if tax_year == 2024:
         return {
             AccountType.Flexible_ISA: ISA_ANNUAL_ALLOWANCE,
-            AccountType.ISA: ISA_ANNUAL_ALLOWANCE,
+            AccountType.Non_Flexible_ISA: ISA_ANNUAL_ALLOWANCE,
             AccountType.Flexible_Lifetime_ISA: LISA_ANNUAL_ALLOWANCE,
         }
     else:
